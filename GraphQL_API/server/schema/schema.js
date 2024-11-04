0e2adbc9a647eb99aa98310f2cf87f781c57ff26
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-11-04 07:57:56 -0600

    task 1 - set up root query

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -2,6 +2,7 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
+    GraphQLSchema
 } = require("graphql");
 
 // Create new GraphQLObjectType
@@ -13,4 +14,27 @@ const TaskType = new GraphQLObjectType({
         weight: { type: GraphQLInt },
         description: { type: GraphQLString }
     })
+});
+
+// Define RootQueryType
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        task: {
+            type: TaskType,
+            args: { id: { type: GraphQLString } },
+            resolve(parent, args) {
+                return {
+                    id: args.id,
+                    title: 'Practice Task',
+                    weight: 5,
+                    description: 'Sample task description'
+                };
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });
