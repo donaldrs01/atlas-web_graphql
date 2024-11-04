f499ae4931db6a1d360f5085774a6752386358dd
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-11-04 14:02:32 -0600

    establish mongoDB connection and create task/project schema

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
+const mongoose = require('mongoose');
 const schema = require('./schema/schema');
 
 const app = express();
+
+// Establish MongoDB connection
+const dbURI = "mongodb+srv://donaldrs:3fHTf9syPHNYcKv@graphql.do4y4.mongodb.net/?retryWrites=true&w=majority&appName=GraphQL";
+
+mongoose.connect(dbURI);
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 app.use('/graphql',graphqlHTTP({
   schema: schema,
