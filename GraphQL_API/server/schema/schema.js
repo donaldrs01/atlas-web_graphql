356c2cad5583b5b92fcd1ccacc9a84b8a79cec29
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-11-04 12:51:25 -0600

    task 3 - created ProjectType object type and project array with dummy data for queries

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -2,11 +2,13 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
-    GraphQLSchema
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLInputObjectType
 } = require("graphql");
 const _ = require('lodash');
 
-// Dummy data
+// Dummy data for 'tasks' type
 const tasks = [
     {
         id: '1',
@@ -22,16 +24,42 @@ const tasks = [
     }
 ];
 
+// Dummy data for 'projects' type
+const projects = [
+    {
+        id: '1',
+        title: 'Advanced HTML',
+        weight: 1,
+        description: 'Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!'
+    },
+    {
+        id: '2',
+        title: 'Bootstrap',
+        weight: 1,
+        description: 'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.'
+    }
+];
+
 // Create new GraphQLObjectType
 const TaskType = new GraphQLObjectType({
     name: 'Task',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         title: { type: GraphQLString },
         weight: { type: GraphQLInt },
         description: { type: GraphQLString }
     })
 });
+
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: () => ({
+        id: { type: GraphQLID },
+        title: { type: GraphQLString },
+        weight: { type: GraphQLInt },
+        description: {type: GraphQLString }
+    })
+})
 
 // Define RootQueryType
 const RootQuery = new GraphQLObjectType({
@@ -39,10 +67,17 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         task: {
             type: TaskType,
-            args: { id: { type: GraphQLString } },
+            args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 // uses lodash to search through tasks and return object with matching ID
                 return _.find(tasks, { id: args.id });
+                }
+            },
+            project: {
+                type: ProjectType,
+                args: { id: {type: GraphQLID } },
+                resolve(parent, args) {
+                    return _.find(projects, { id: args.id });
                 }
             }
         }
